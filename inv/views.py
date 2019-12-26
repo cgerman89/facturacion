797d0f3b90f79543a328bf44351f7e0cacf188d7
Crstian German <cgerman.89@outlook.com>
2019-12-26 00:05:59 -0500

    listo inv-categorias

--- inv/views.py
+++ inv/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Categoria
+from .form import CategoriaForm
 
 
 # Create your views here.
@@ -9,4 +11,38 @@ class CategoriaView(LoginRequiredMixin, generic.ListView):
     model = Categoria
     template_name = 'inv/categoria_list.html'
     context_object_name = 'obj'
-    login_url = 'core:'
+    login_url = 'core:login'
+
+
+class CategoriaNew(LoginRequiredMixin, generic.CreateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_object_name = 'obj'
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'core:login'
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_object_name = 'obj'
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'core:login'
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+
+class CategoriaDelete(LoginRequiredMixin, generic.DeleteView):
+    model = Categoria
+    template_name = 'inv/categoria_delete.html'
+    context_object_name = 'obj'
+    success_url = reverse_lazy('inv:categoria_list')
+
